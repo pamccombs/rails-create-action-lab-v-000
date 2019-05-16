46561c3cb324b26c83d77d53919fedc94b13ffd5
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-16 22:08:12 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
   def create
     @student= Student.new(first_name: params[:first_name],
         last_name: params[:last_name])
-    redirect_to 
+    redirect_to student_path(@student)
   end
 
 end
